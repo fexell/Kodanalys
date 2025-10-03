26f0494740e88c9afe789d28f841dc649f3a8a64
Felix Cervin <fexellofficial@gmail.com>
2025-10-03 15:19:45 +0200

    docs: Commented Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace Kodanalys {
     internal class Menu {
+
+        // Menyval och deras tillhörande åtgärder
         static Dictionary<int, MenuItem> menuItems = new() {
             { 1, new MenuItem( "Lägg till användare", () => User.AddUser() ) },
             { 2, new MenuItem( "Visa alla användare", () => User.ListUsers() ) },
@@ -14,6 +16,9 @@ namespace Kodanalys {
             { 5, new MenuItem( "Avsluta", () => Exit() ) },
         };
 
+        /// <summary>
+        /// Visar meny
+        /// </summary>
         public static void ShowMenu() {
             Console.WriteLine( "===== Meny ======" );
             Console.WriteLine( "1. Lägg till användare" );
@@ -24,6 +29,9 @@ namespace Kodanalys {
             Console.WriteLine( "=================" );
         }
 
+        /// <summary>
+        /// Loopar menyn
+        /// </summary>
         public static void MenuLoop() {
             while( true ) {
                 Console.Clear();
@@ -39,21 +47,44 @@ namespace Kodanalys {
             }
         }
 
+        /// <summary>
+        /// Avslutar programmet
+        /// </summary>
         public static void Exit() {
             Environment.Exit( 0 );
         }
     }
 
+    /// <summary>
+    /// Menyval-klassen
+    /// </summary>
     public class MenuItem {
+
+        // Namn på menyval
         public string Name { get; set; }
+
+        // Funktionen/action som ska köras vid menyval
         public Action Action { get; set; }
 
+        /// <summary>
+        /// Skapar ny menyval
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="action"></param>
+        /// <param name="isReturn"></param>
         public MenuItem( string name, Action action, bool isReturn = true ) {
+
+            // Sätt namn
             Name = name;
-            
-            if( isReturn ) {
+
+            // Sätt action, med eller utan "tryck enter för att fortsätta"
+            if ( isReturn ) {
                 Action = () => {
+
+                    // Kör action
                     action.Invoke();
+
+                    // Vänta på inmatning innan menyn visas igen
                     Console.WriteLine( "\nTryck enter för att fortsätta..." );
                     Console.ReadKey();
                 };
