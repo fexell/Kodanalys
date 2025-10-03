48d03ff31a37e2282febdf446017c4a2a469109e
Felix Cervin <fexellofficial@gmail.com>
2025-10-03 08:52:09 +0200

    feat: Helpers.cs -> Created ValidateInt method

--- Helpers.cs
+++ Helpers.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace Kodanalys {
     public class Helpers {
-
+        public static int ValidateInt( string input ) {
+            while( true ) {
+                if ( int.TryParse( input, out int result ) ) {
+                    return result;
+                } else {
+                    Console.Write( "Ogiltig inmatning. Försök igen: " );
+                    input = Console.ReadLine();
+                }
+            }
+        }
     }
 }
